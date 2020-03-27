0de4e85c4d3702906c1efda158e889d87d8a01bd
Karl Gordon <kgordon@stsci.edu>
2020-03-26 22:02:51 -0400

    updated plots

--- plot_ext_litcomp.py
+++ plot_ext_litcomp.py
@@ -4,7 +4,17 @@ import numpy as np
 import matplotlib.pyplot as pyplot
 import matplotlib
 from astropy.table import Table
-import astropy.units as u
+
+# import astropy.units as u
+
+# from dust_extinction.shapes import P92
+from dust_extinction.averages import (
+    RL85_MWAvg,
+    I05_MWAvg,
+    CT06_MWGC,
+    CT06_MWLoc,
+    F11_MWGC,
+)
 
 from measure_extinction.extdata import ExtData
 
@@ -44,9 +54,7 @@ if __name__ == "__main__":
     parser.add_argument("--pdf", help="save figure as a pdf file", action="store_true")
     args = parser.parse_args()
 
-    avefilename = "test.fits"
-
-    fontsize = 18
+    fontsize = 14
 
     font = {"size": fontsize}
 
@@ -59,94 +67,59 @@ if __name__ == "__main__":
     matplotlib.rc("ytick.major", width=2)
     matplotlib.rc("ytick.minor", width=2)
 
-    figsize = (10, 8)
-    fig, ax = pyplot.subplots(nrows=1, ncols=1, figsize=figsize)
+    figsize = (14, 6)
+    fig, ax = pyplot.subplots(nrows=1, ncols=2, figsize=figsize)
 
-    # Milky Way observed extinction from
-    # Rieke & Lebofsky (1985)
-    GL85_wave = np.array(
-        [
-            13.0,
-            12.5,
-            12.0,
-            11.5,
-            11.0,
-            10.5,
-            10.0,
-            9.5,
-            9.0,
-            8.5,
-            8.0,
-            4.8,
-            3.5,
-            2.22,
-            1.63,
-            1.23,
-        ]
-    )
-    GL85_axav = np.array(
-        [
-            0.027,
-            0.030,
-            0.037,
-            0.047,
-            0.060,
-            0.074,
-            0.083,
-            0.087,
-            0.074,
-            0.043,
-            0.020,
-            0.023,
-            0.058,
-            0.112,
-            0.175,
-            0.282,
-        ]
-    )
-    if args.elkejk:
-        GL85_y, ji, ki = get_elkejk_from_alav(GL85_wave, GL85_axav)
-    else:
-        GL85_y = GL85_axav
-    ax.plot(GL85_wave, GL85_y, "bo", label="GalCenter; Rieke & Lebofsky (1985)")
+    litmods = [RL85_MWAvg(), I05_MWAvg(), CT06_MWGC(), CT06_MWLoc(), F11_MWGC()]
+    litdesc = [
+        "RL85_MWAvg: Rieke & Lebofsky (1985)",
+        "I05_MWAvg: Indebetouw et al. (2005)",
+        "CT06_MWGC: Chiar & Tielens (2006)",
+        "CT06_MWLocal: Chiar & Tielens (2006)",
+        "F11_MWGC: Fritz et al. (2011)",
+    ]
+    litfmt = ["bP", "gP", "c--", "c-.", "mP"]
+    for k, cmod in enumerate(litmods):
+        lit_wave = 1.0 / cmod.obsdata_x
+        lit_axav = cmod.obsdata_axav
 
-    # Chiar & Tielens (2005)
-    # table in A(l)/A(K) units
-    CT05 = Table.read(
-        "data/pixie_dust_chiar_2005_modified.dat", format="ascii.commented_header"
-    )
-    CT05_wave = CT05["wave"]
-    if args.elkejk:
-        CT05_y, ji, ki = get_elkejk_from_alav(CT05_wave, 0.12 * CT05["local"])
-    else:
-        CT05_y = 0.12 * CT05["local"]
-    ax.plot(CT05_wave, CT05_y, "co", label="GalCenter; Chiar & Tielens (2005)")
+        lit_y, ji, ki = get_elkejk_from_alav(lit_wave, lit_axav)
+        ax[1].plot(
+            lit_wave,
+            lit_axav / lit_axav[ki],
+            litfmt[k],
+            alpha=0.25,
+            label=litdesc[k],
+            lw=3,
+        )
+        ax[0].plot(lit_wave, lit_y, litfmt[k], alpha=0.25, lw=3)
 
     # Zasowski et al. 2009 (supercedes Indebetouw?)
-    # for l=90
     Z09_wave = np.array([1.22, 1.63, 2.22, 3.6, 4.5, 5.8, 8.0])
     Z09_ehlehk_l10 = np.array([-1.97, 0.0, 1.0, 1.79, 1.92, 2.08, 2.00])
     Z09_ehlehk_l90 = np.array([-2.18, 0.0, 1.0, 1.94, 2.38, 2.78, 2.49])
-    if args.elkejk:
-        Z09_y_l10 = get_elkejk_from_ehlehk(Z09_wave, Z09_ehlehk_l10)
-        Z09_y_l90 = get_elkejk_from_ehlehk(Z09_wave, Z09_ehlehk_l90)
-        ax.plot(
-            Z09_wave,
-            Z09_y_l10,
-            "go",
-            fillstyle="none",
-            markersize=10,
-            markeredgewidth=2.0,
-            label="GalPlane (l10-15); Zasowski et al. (2009)",
-        )
-        ax.plot(
-            Z09_wave,
-            Z09_y_l90,
-            "go",
-            markersize=10,
-            markeredgewidth=2.0,
-            label="GalPlane (l90); Zasowski et al. (2009)",
-        )
+
+    Z09_y_l10 = get_elkejk_from_ehlehk(Z09_wave, Z09_ehlehk_l10)
+    Z09_y_l90 = get_elkejk_from_ehlehk(Z09_wave, Z09_ehlehk_l90)
+    ax[0].plot(
+        Z09_wave,
+        Z09_y_l10,
+        "rs",
+        fillstyle="none",
+        markersize=10,
+        markeredgewidth=1.0,
+        alpha=0.5,
+        label="GalPlane (l10-15); Zasowski et al. (2009)",
+    )
+    ax[0].plot(
+        Z09_wave,
+        Z09_y_l90,
+        "rs",
+        markersize=10,
+        markeredgewidth=1.0,
+        alpha=0.5,
+        label="GalPlane (l90); Zasowski et al. (2009)",
+    )
 
     # Xue et al. 2016
     X16_wave = np.array(
@@ -168,62 +141,72 @@ if __name__ == "__main__":
             0.0,
         ]
     )
-    if args.elkejk:
-        X16_y = -1 * X16_eklejk
-        ax.plot(X16_wave, X16_y, "ro", label="GalPlane; Xue et al. (2016)")
+    X16_y = -1 * X16_eklejk
+    ax[0].plot(X16_wave, X16_y, "yP", label="GalPlane; Xue et al. (2016)")
 
-    # Fritz et al. 2011
-    F11 = Table.read("data/fritz11_galcenter.dat", format="ascii.commented_header")
-    F11_wave = F11["wave"]
-    if args.elkejk:
-        F11_y, ji, ki = get_elkejk_from_alav(F11_wave, 0.12 * F11["ext"] / 2.62)
-    else:
-        F11_y = 0.12 * F11["ext"] / 2.62
-    ax.plot(F11_wave, F11_y, "mo", label="GalCenter; Fritz et. al. (2011)")
+    # New measurements
+    avefilename = "data/all_ext_18feb20_ave.fits"
 
-    # get G19
-    G19 = ExtData()
-    G19.read(avefilename)
-    G19_wave = G19.waves["BAND"].value
-    if args.elkejk:
-        G19_y, ji, ki = get_elkejk_from_alav(G19_wave, G19.exts["BAND"])
-    else:
-        G19_y = G19.exts["BAND"]
-    ax.plot(
-        G19_wave,
-        G19_y,
-        "ko",
+    # get G20_MWAvg
+    G20 = ExtData()
+    G20.read(avefilename)
+    G20_wave = G20.waves["BAND"].value
+
+    G20_ext = G20.exts["BAND"]
+    G20_ext_uncs = G20.uncs["BAND"]
+
+    gindxs_IRS = np.where(G20.npts["IRS"] > 0)
+    G20_IRS_wave = G20.waves["IRS"][gindxs_IRS].value
+    G20_IRS_ext = G20.exts["IRS"][gindxs_IRS]
+    G20_IRS_uncs = G20.uncs["IRS"][gindxs_IRS]
+
+    # photometry
+    G20_y, ji, ki = get_elkejk_from_alav(G20_wave, G20_ext)
+
+    ax[1].errorbar(
+        G20_wave,
+        G20_ext / G20_ext[ki],
+        yerr=G20_ext_uncs,
+        fmt="ko",
         markersize=12,
-        markeredgewidth=2.0,
-        label="Diffuse ISM; this work",
+        markeredgewidth=1.0,
+        alpha=0.5,
+        label="this work (JHK, IRAC, IRS15, MIPS24)",
     )
-    # plot the IRS data
-    G19_IRS_y = G19.exts["IRS"]
-    if args.elkejk:
-        G19_IRS_y = G19_IRS_y / (G19.exts["BAND"][ji] - G19.exts["BAND"][ki])
-        G19_akejk = G19.exts["BAND"][ki] / (G19.exts["BAND"][ji] - G19.exts["BAND"][ki])
-        G19_IRS_y = G19_IRS_y - G19_akejk
-    gindxs = np.where(G19.npts["IRS"] > 0)
-    ax.plot(G19.waves["IRS"][gindxs], G19_IRS_y[gindxs], "k-")
+
+    ax[0].plot(
+        G20_wave, G20_y, "ko", markersize=12, markeredgewidth=1.0, alpha=0.5,
+    )
+
+    # IRS
+    ax[1].plot(
+        G20_IRS_wave, G20_IRS_ext / G20_ext[ki], "k-", label="this work (IRS)", lw=2, alpha=0.65
+    )
+
+    G20_IRS_y = G20_IRS_ext / (G20_ext[ji] - G20_ext[ki])
+    G20_akejk = G20_ext[ki] / (G20_ext[ji] - G20_ext[ki])
+    G20_IRS_y = G20_IRS_y - G20_akejk
+    ax[0].plot(G20_IRS_wave, G20_IRS_y, "k-", lw=2, alpha=0.65)
+
+    for i in range(2):
+        ax[i].set_yscale("linear")
+        ax[i].set_xscale("log")
+        ax[i].set_xlabel(r"$\lambda$ [$\mu m$]")
+        ax[i].tick_params("both", length=10, width=2, which="major")
+        ax[i].tick_params("both", length=5, width=1, which="minor")
+
+        ax[i].legend(fontsize=0.75 * fontsize)
 
     # finishing plot details
-    if args.elkejk:
-        ax.set_xlim(1.0, 40.0)
-        ax.set_ylim(-1.0, 1.1)
-        ax.set_ylabel(r"$E(\lambda - K)/E(J - K)$", fontsize=1.3 * fontsize)
-    else:
-        ax.set_ylim(0.0, 0.25)
-        ax.set_ylabel(r"$A(\lambda)/A(V)$", fontsize=1.3 * fontsize)
+    ax[0].set_xlim(1.0, 40.0)
+    ax[0].set_ylim(-0.8, 1.1)
+    ax[0].set_ylabel(r"$E(\lambda - K)/E(J - K)$", fontsize=1.3 * fontsize)
 
-    ax.set_yscale("linear")
-    ax.set_xscale("log")
-
-    ax.set_xlabel(r"$\lambda$ [$\mu m$]")
-
-    ax.tick_params("both", length=10, width=2, which="major")
-    ax.tick_params("both", length=5, width=1, which="minor")
-
-    ax.legend(fontsize=fontsize)
+    ax[1].set_xlim(1.0, 40.0)
+    ax[1].set_ylim(0.0, 3.0)
+    ax[1].set_ylabel(r"$A(\lambda)/A(K)$", fontsize=1.3 * fontsize)
+    ax[1].yaxis.tick_right()
+    ax[1].yaxis.set_label_position("right")
 
     fig.tight_layout()
 
